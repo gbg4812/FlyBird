1a9ac28a717c8dd628255b4fd02c0f41e7db83f8
gbg4812 <guillembaldi@gmail.com>
2026-01-22 11:52:43 +0100

    Agregar archivos de proyecto.

---
+++ FlyBird.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "raylib.h"
+
+using namespace std;
+
+int main()
+{
+	InitWindow(800, 600, "Fly Bird");
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawCircle(400, 300, 200, BLUE);
+
+		EndDrawing();
+	}
+	return 0;
+}
